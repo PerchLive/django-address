13be9e6be598213bd3bb968ee6fd4722b4e69943
awh4kc <awh4kc@virginia.edu>
2020-03-27 10:41:05 -0400

    add new dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'six'],
     zip_safe=False,
 
 )
